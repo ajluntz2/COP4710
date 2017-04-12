4a2aa1440cf344247e7eed392a78ec018c624d1e
ajluntz2 <ajluntz2@users.noreply.github.com>
2017-04-12 18:22:11 -0400

    untested event object

---
+++ php/event.php
@@ -0,0 +1,97 @@
+<?php
+require_once('db_table.php');
+
+class event_info extends database_table
+{
+  public $id = -1;
+  public $name = '';
+  public $adminid = -1;
+  public $locationid = -1;
+  public $rsoid = -1;
+  public $universityid = -1;
+  public $ratingid = -1;
+  public $catigory = '';
+  public $description = '';
+  public $startdate = '';
+  public $time = '';
+  public $length = '';
+  public $days = '';
+  public $enddate = '';
+  public $fequency = '';
+
+  function __construct()
+  {
+    $this->table = 'events';
+  }
+
+  function addEvent($userid, $universityid, $locationid,
+                    $rsoid, $ratingid, $category,
+                    $description, $startdate, $enddate,
+                    $days, $time, $length,
+                    $frequency)
+  {
+    $insert_query = "
+    INSERT INTO events(  name   ,    adminid  ,    locationid  ,    rsoid  ,    universityid  ,    ratingid  ,    category  ,    description  ,    startdate  ,    time  ,    length  ,    days  ,    enddate  ,    frequency)
+    VALUES            (".$name.", ".$adminid.", ".$locationid.", ".$rsoid.", ".$universityid.", ".$ratingid.", ".$category.", ".$description.", ".$startdate.", ".$time.", ".$length."), ".$days.", ".$enddate.", ".$frequency.")
+    ";
+
+    $this->query($insert_query);
+
+    $event = new event_info();
+    if (!$event->updateOnName($name))
+    {
+      return null;
+    }
+    return $event;
+  }
+
+  function updateOnName($name)
+  {
+    $query = "SELECT * FROM events WHERE name = ".$name;
+    $row = $this->simpleQuery($query);
+    if ($row !== null)
+    {
+      $this->updateFields($row);
+      return true;
+    }
+    return false;
+  }
+
+  function updateOnId($id)
+  {
+    $this->id = $id;
+    return $this->update();
+  }
+
+  function update()
+  {
+    $query = "SELECT * FROM events WHERE eventid = ".$this->id;
+    $row = $this->simpleQuery($query);
+    if ($row !== null)
+    {
+      $this->updateFields($row);
+      return true;
+    }
+    return false;
+  }
+
+  private function updateFields($row)
+  {
+    $this->id = $row['userid'];
+    $this->name = $row['name'];
+    $this->adminid = $row['adminid'];
+    $this->locationid = $row['locationid'];
+    $this->rsoid = $row['rsoid'];
+    $this->universityid = $row['universityid'];
+    $this->ratingid = $row['ratingid'];
+    $this->catigory = $row['catigory'];
+    $this->description = $row['description'];
+    $this->startdate = $row['startdate'];
+    $this->time = $row['time'];
+    $this->length = $row['length'];
+    $this->days = $row['days'];
+    $this->enddate = $row['enddate'];
+    $this->fequency = $row['fequency'];
+  }
+}
+?>
